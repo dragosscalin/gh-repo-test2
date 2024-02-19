fdf602976234d133a01a75571e31e34323471e7d
dragosscalin <119862119+dragosscalin@users.noreply.github.com>
2024-02-19 17:55:11 +0200

    Update HelloWorldModel.java

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -35,7 +35,7 @@ import java.util.Optional;
 @Model(adaptables = Resource.class)
 public class HelloWorldModel {
 
-    public static final PASSWORD="pass";
+    public static final String PASSWORD="pass";
     @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
     @Default(values="No resourceType")
     protected String resourceType;
